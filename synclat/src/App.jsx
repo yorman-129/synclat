6176604add8b52c65e3861f39990abc301b2475c
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2025-04-07 09:06:49 -0500

    Implementing (Fix): I install react-router-dom and create routes.

--- synclat/src/App.jsx
+++ synclat/src/App.jsx
@@ -6,7 +6,6 @@ import { AuthPage } from "./Pages/AuthPage";
 function App() {
   return (
     <Routes>
-      <Route path="" element={<HomePage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/auth" element={<AuthPage />} />
     </Routes>
